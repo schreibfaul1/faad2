1b26e79e2b108e6b393224e8d459c907152f9c23
schreibfaul1 <schreibfaul1@t-online.de>
2023-08-31 22:28:10 +0200

    fix outBuff size

--- src/main.cpp
+++ src/main.cpp
@@ -45,9 +45,9 @@ int main() {
 	printf("channels = %i\n", channels);
 
 	const char* sr;
-	if(samplerate == 44100) sr = "\x44\xAC\x00\x00";
+	if(samplerate == 44100)      sr = "\x44\xAC\x00\x00";
 	else if(samplerate == 48000) sr = "\x80\xBB\x00\x00";
-	else if(samplerate == 22050) sr = "\x80\xBB\x00\x00";
+	else if(samplerate == 22050) sr = "\x22\x56\x00\x00";
 	else {printf("wrong samplerate\n"); return 0;}
 
 	const char* ch;
@@ -64,8 +64,8 @@ int main() {
 	fwrite("\x01\x00", 2, 1, wavFile);          // AudioFormat
 	fwrite(ch        , 2, 1, wavFile);          // NumChannels
 	fwrite(sr        , 4, 1, wavFile);          // Sample Rate
-	fwrite(sr        , 4, 1, wavFile);          // Byte Rate
-	fwrite("\x02\x00", 2, 1, wavFile);          // Block Align
+	fwrite("\x00\x00\x00\x00", 4, 1, wavFile);  // Byte Rate
+	fwrite("\x04\x00", 2, 1, wavFile);          // Block Align
 	fwrite("\x10\x00", 2, 1, wavFile);          // Bits Per Sample
 	fwrite("data", 4, 1, wavFile);              // Subchunk 2 ID
 	fwrite("\x00\x00\x00\x00", 4, 1, wavFile);  // Subchunk 2 Size (will be filled later)
@@ -73,19 +73,26 @@ int main() {
 
 	// decode file
 	size_t pos = 0;
-	int i = 0;
+	int loopidx = 0;
 	uint16_t bytesconsumed = 0;
 	while(1){
 		fseek(aacFile, pos, SEEK_SET);
 		bytesRead = fread(inBuffer, 1, sizeof(inBuffer), aacFile);
 		if(bytesRead < 1) break;
-		NeAACDecDecode2(hAac, &frameInfo, inBuffer, sizeof(inBuffer), (void**)&ob, bytesRead);
+		NeAACDecDecode2(hAac, &frameInfo, inBuffer, sizeof(inBuffer), (void**)&ob, sizeof(outBuffer));
 		bytesconsumed = frameInfo.bytesconsumed;
-    	printf("bytesconsumed %i \n", bytesconsumed);
+    	// printf("bytesconsumed %i \n", bytesconsumed);
 		pos += bytesconsumed;
 		if(bytesconsumed == 0) break;
-		printf("samples %ld\n", frameInfo.samples);
+		// printf("samples %ld\n",frameInfo.samples);
 		fwrite(ob, 1, frameInfo.samples * frameInfo.channels, wavFile);
+		if(loopidx == 1){
+			printf("Parametric Stereo %d (0 off, 1 on)\n", frameInfo.ps);
+			printf("AAC Header Type %d (0 raw, 1 ADIF, 2 ADTS)\n",frameInfo.header_type);
+			printf("Spectral Band Replication %d (0 no SBR, 1 upsampled SBR, 2 downsampled SBR, 3 no SBR but upsampled)\n",frameInfo.sbr);
+		}
+		if(frameInfo.error) printf("err %s\n", NeAACDecGetErrorMessage(frameInfo.error));
+		loopidx++;
 	}
 
 	printf("ready\n");
